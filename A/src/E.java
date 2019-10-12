d590efb6d2ed7c520bec16c0551dee0ed4f4456e
sagar korde <sagargk1@gmailcom>
2019-10-12 17:04:15 +0530

    hi, my first commit

---
+++ A/src/E.java
@@ -0,0 +1,9 @@
+
+public class E {
+public static void main(String[] args) {
+	System.out.println(3/2);
+	System.out.println("3/2.0="+(3/2.0));
+	System.out.println("3.0/2="+(3.0/2));
+    System.out.println("3.0/2.0="+(3.0/2.0));
+}
+}
